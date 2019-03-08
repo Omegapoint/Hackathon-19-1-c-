f07ced0dcc75186b6a2bd3fb29aa804b2c8e9ad1
Filip Mattsson <filmat@omegapoint.se>
2019-03-08 15:09:03 +0100

    main class and player

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "player.h"
+
+int main() {
+	Player player;
+
+	player.fullness = 2;
+
+	player.printHunger();
+}
---
+++ player.h
@@ -0,0 +1,15 @@
+#include <iostream>
+class Player {
+    public:
+
+    int fullness;
+	int profit;
+
+    void printHunger() {
+        std::cout << fullness;
+    }
+
+	void eat() {
+
+	}
+};
