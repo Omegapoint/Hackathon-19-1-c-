13c7e46919b7d784d6c5fdd2ce32d45e5c397867
Filip Mattsson <filmat@omegapoint.se>
2019-03-08 17:10:07 +0100

    user input in game loop

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ int main() {
 	BuffeeTable tb = BuffeeTable(5);
 
 	int round = 0;
+	string input;
 	while (person.timeLeft > 0) {
 		printf("Round %d \n", round);
+
+		cin >> input;
+		cout << input;
 
 		person.timeLeft -= 1;
 		round++;
